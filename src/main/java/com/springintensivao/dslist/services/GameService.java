5285feb645981491c38757fc1f72bcf0e330f0c8
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2023-11-15 15:54:06 -0300

    SQL queryro, projection, search games by list

--- src/main/java/com/springintensivao/dslist/services/GameService.java
+++ src/main/java/com/springintensivao/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.springintensivao.dslist.dto.GameDTO;
 import com.springintensivao.dslist.dto.GameMinDTO;
 import com.springintensivao.dslist.entities.Game;
+import com.springintensivao.dslist.projections.GameMinProjection;
 import com.springintensivao.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
